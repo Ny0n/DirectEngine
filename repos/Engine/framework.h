4ab8d4d0df791f6533d187f007de366469f3a479
Killian <killian.perez64@gmail.com>
2022-03-08 15:27:12 +0100

    Initial commit

---
+++ repos/Engine/framework.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#pragma comment(lib, "d3d9.lib")
+#ifdef _DEBUG
+	#pragma comment(lib, "d3dx9d.lib")
+#else
+	#pragma comment(lib, "d3dx9.lib")
+#endif // _DEBUG
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// 3D
+#include <d3dx9.h>
+
+ // Windows
+#include <windows.h>
+
+// C Runtime Header Files
+#include <stdlib.h>
+#include <stdio.h>
+#include <io.h>
+#include <math.h>
