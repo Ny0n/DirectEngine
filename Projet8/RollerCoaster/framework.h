2174ee6f7423b74d47cbec9b755b48c8f76098da
Ny0n <flav38320@gmail.com>
2022-03-10 14:20:59 +0100

    tuto win 1-4 & tuto dx 1

--- Projet8/RollerCoaster/framework.h
+++ Projet8/RollerCoaster/framework.h
@@ -8,8 +8,15 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+#include <windowsx.h>
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
+#include <d3d9.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d9.lib")
+
