0c37b59e0cf49a2b79a626ac41898a1ef0387f53
LionelJ <ljanin@gaming.tech>
2022-03-18 15:59:11 +0100

    start adding texture (still not working)

--- Projet8/Engine/MeshRenderer.h
+++ Projet8/Engine/MeshRenderer.h
@@ -15,6 +15,8 @@ public:
 
 private:
     ID3DXMesh* _pmesh;
+    D3DMATERIAL9* g_pMeshMaterials = NULL; // Materials for our mesh
+    LPDIRECT3DTEXTURE9* g_pMeshTextures = NULL; // Textures for our mesh
     LPCWSTR _path;
-
+    DWORD  g_dwNumMaterials = 0L;
 };
