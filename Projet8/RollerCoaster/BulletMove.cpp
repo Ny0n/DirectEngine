130100f57a112615d6298d903aae8d675b5b65ef
Lionel <li.janin@laposte.net>
2022-03-24 23:36:50 +0100

    start adding bullet

---
+++ Projet8/RollerCoaster/BulletMove.cpp
@@ -0,0 +1,28 @@
+﻿#include "BulletMove.h"
+
+
+// Start is called before the first frame update
+void BulletMove::Start()
+{
+	currentTimer = maxTimer;
+}
+
+// Update is called once per frame
+void BulletMove::Update()
+{
+	auto dir = transform->GetForward();
+
+	D3DXVECTOR3 pos = transform->GetPosition();
+	pos += dir * bulletSpeed * Time::deltaTime;
+
+	transform->SetPosition(pos);
+
+	if(currentTimer < 0)
+	{
+		//gameObject->Destroy(); // throw an exception :/
+	}else
+	{
+		currentTimer -= Time::deltaTime;
+	}
+
+}
---
+++ Projet8/RollerCoaster/BulletMove.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "EngineLib.h"
+
+class BulletMove : public MonoBehaviour
+{
+public:
+	string GetType() override { return NAMEOF(BulletMove); }
+	void Start() override;
+	void Update() override;
+
+	void SetBulletSpeed(float speed) { bulletSpeed = speed; }
+	void SetmaxTimer(float time) { maxTimer = time; }
+
+private:
+	float bulletSpeed = 10.0f;
+	float maxTimer = 5.0f; //time before destroy
+	float currentTimer;
+};
