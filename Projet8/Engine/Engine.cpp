ab8ee07a1ae47b16b5d2d3255cc257279125f23b
Killian <32911550+Meshanda@users.noreply.github.com>
2022-03-31 11:42:44 +0200

    nice flag to get +120 fps

--- Projet8/Engine/Engine.cpp
+++ Projet8/Engine/Engine.cpp
@@ -44,7 +44,7 @@ void Engine::InitD3D()
     d3d->CreateDevice(D3DADAPTER_DEFAULT,
         D3DDEVTYPE_HAL,
         _window,
-        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_PUREDEVICE,
         &d3dpp,
         &d3ddev);
     
